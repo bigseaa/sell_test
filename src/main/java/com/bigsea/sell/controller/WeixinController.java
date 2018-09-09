b12edf558cf47ff1a90f0341b4baa2fb9e3604c9
bigseaa <506798278@qq.com>
2018-09-09 16:10:48 +0800

    20180909

---
+++ src/main/java/com/bigsea/sell/controller/WeixinController.java
@@ -0,0 +1,25 @@
+package com.bigsea.sell.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/weixin")
+@Slf4j
+public class WeixinController {
+
+    @GetMapping("/auth")
+    public void auth(@RequestParam("code") String code) {
+        log.info("进入auth方法。。。");
+        log.info("code={}", code);
+
+        String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx4d1ba071d1cb8f2a&secret=20df9a9a96b29ad6395e7a657c2fc99a&code=" + code + "&grant_type=authorization_code";
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(url, String.class);
+        log.info("response={}", response);
+    }
+}
